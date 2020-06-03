47fa807b3b57b008212c3ac33e65bf028a52b932
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-06-03 12:13:03 -0500

    [Update]: Basic template

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-//ort { Router, Link } from "@reach/router";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Home } from './Home'
-import { About } from './About'
-import { Contact } from './Contact'
-import { NoMatch } from './NoMatch'
-import { Layout } from './components/Layout'
-import { NavigationBar } from './components/NavigationBar'
-import { Jumbotron } from './components/Jumbotron';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Contact } from "./Contact";
+import { NoMatch } from "./NoMatch";
+import { Layout } from "./components/Layout";
+import { NavigationBar } from "./components/NavigationBar";
+import { Jumbotron } from "./components/Jumbotron";
 
+console.warn = () => {};
 class App extends Component {
   render() {
     return (
@@ -19,12 +19,12 @@ class App extends Component {
           <NavigationBar />
           <Jumbotron />
           <Layout>
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/about" component={About} />
-                  <Route path="/contact" component={Contact} />
-                  <Route component={NoMatch} />
-                </Switch>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
           </Layout>
         </Router>
       </React.Fragment>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,44 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap'
-import styled from 'styled-components'
-
-// const Styles = styled.div`
-//   .navbar {
-//     background-color: #5E3023;
-//   }
-//   a, .navbar-brand, .navbar-nav .nav-link {
-//     color: #bbb;
-//     &:hover {
-//       color: white;
-//     }
-//   }
-// `;
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
 
 export const NavigationBar = () => (
-    <div>
-        <Navbar expand="lg">
-            <Navbar.Brand href="/">PetSpot</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-                <Nav.Item>
-                <Nav.Link>
-                    <Link to="/about">About us</Link>
-                </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link>
-                    <Link to="/about">Sign up</Link>
-                </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link>
-                    <Link to="/contact">Login</Link>
-                </Nav.Link>
-                </Nav.Item>
-            </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </div >
-  )
+  <div>
+    <Navbar expand="lg">
+      <div>
+        <Navbar.Brand href="/" className="logo"></Navbar.Brand>
+      </div>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Link className="item_pad" to="/about">
+            About us
+          </Link>
+          <Link className="item_pad" to="/about">
+            Sign up
+          </Link>
+          <Link className="item_pad" to="/contact">
+            Login
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </div>
+);
