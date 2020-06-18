fff8027a695be6094588abcac61551c0f8397349
felipemontes <felipe_londono1@hotmail.com>
2020-06-18 17:57:18 -0500

    [Add]: styling website

--- src/Pets.js
+++ src/Pets.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "@reach/router";
 import { Card, Button } from "react-bootstrap";
 
-export default function Pets({ id, name, description, picture }) {
+export default function Pets({ id, name, picture, sex, breed }) {
   return (
     <div className="sep">
       <Card>
         <Card.Img className="image-container" variant="top" src={picture} />
         <Card.Body className="custom-card">
           <Card.Title>{name}</Card.Title>
-          <Card.Text>{description}</Card.Text>
+          <Card.Text>{breed} | {sex}</Card.Text>
           <Link to={`/pet/${id}`}>
             <Button onClick variant="outline-dark">
               More about me
