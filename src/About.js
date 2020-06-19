974f5a5f5a10d49e553f8b6c99e0e3b59c10765b
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-06-18 23:41:13 -0500

    [Fix]: Typo

--- src/About.js
+++ src/About.js
@@ -16,7 +16,11 @@ export const About = () => (
       </Col>
       <Col xs={12} sm={7} md={6} lg={6}>
         <p className="intro">
-          PetSpot is an online platform where users can post pets for adoption. We want to make the process of adoption as easy as possible and contribute to the adoption of as many pets as possible. Every user can register and post a pet to adopt including name, photo, breed, sex, and a simple description of it.
+          PetSpot is an online platform where users can post pets for adoption.
+          We want to make the process of adoption easy and contribute to it
+          finding a home for as many pets as possible. Every user can register
+          and post a pet to adopt including name, photo, breed, sex, and a
+          simple description of it.
         </p>
       </Col>
     </Row>
@@ -26,9 +30,11 @@ export const About = () => (
           <b>Our mission</b>
         </h2>
         <p className="other">
-          Our mission is to create a digital space for interaction between people and shelters. This enables pets adoption,
-          especially those in precarious conditions. We aim to provide a clear connection between pets that need a new home
-          and people who are looking to responsibly adopt a new pet, always prioritizing the pet's well-being.
+          Our mission is to create a digital space for interaction between
+          people and shelters. This enables pets adoption, especially those in
+          precarious conditions. We aim to provide a clear connection between
+          pets that need a new home and people who are looking to responsibly
+          adopt a new pet, always prioritizing the pet's well-being.
         </p>
       </Col>
       <Col xs={6} md={4} lg={6}>
@@ -52,12 +58,13 @@ export const About = () => (
           <b>Our vision</b>
         </h2>
         <p className="other">
-          Our vision is to turn PetsPot in 5 years into an international benchmark of pet care institutions.
-          Where there is the possibility that anyone can be a sponsor of one or more pets, even in other countries.
-          The sponsor can play with their pets, take care of them and even adopt them, this way we can contribute to the
-          reduction of stray animals and improve their life's quality, and give our contribution to the social issues
-          in the community
-
+          Our vision is to turn PetsPot in 5 years into an international
+          benchmark of pet care institutions. Where there is the possibility
+          that anyone can be a sponsor of one or more pets, even in other
+          countries. The sponsor can play with their pets, take care of them and
+          even adopt them, this way we can contribute to the reduction of stray
+          animals and improve their life's quality, and give our contribution to
+          the social issues in the community
         </p>
       </Col>
     </Row>
